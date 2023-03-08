865112eca366a113338f9e335f7abb620bfab8f7
Kade <kadekorn@gmail.com>
2023-03-07 21:27:22 -0700

    Finshed up on the shapes and javascript. It was pretty fun. Now on to making it responsive. Remember to add the popper to this. Take away the code from previous work

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20; // writing learn code
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor; // / 20;
+  const y = event.clientY * scaleFactor; // / 20;
 
-function toggleContrast(){
-    contrastToggle = !contrastToggle;
-    if(contrastToggle){
-        document.body.classList += " dark-theme";
-    }
-    else {
-        document.body.classList.remove("dark-theme")
-    }
-
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
 }
 
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
   event.preventDefault(); // this stops form from immediately refreshing the page
@@ -25,26 +34,26 @@ function contact(event) {
       "template_bjrp5fk",
       event.target,
       "uJjAf7G8kBbZURrbj"
-    ).then(() => {
+    )
+    .then(() => {
       //throw new Error("error") // <-- this is for testing out error feature
       loading.classList.remove("modal__overlay--visible");
       success.classList += " modal__overlay--visible";
-    }).catch(() => {
-        loading.classList.remove("modal__overlay--visible");
-        alert(
-            "The email service is temporarily unavailable. Please contanct me directly at 'kadekorn@.com'"
-        )
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contanct me directly at 'kadekorn@.com'"
+      );
     });
 }
 
-
-
-function toggleModal(){
-    if (isModalOpen){
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open")
-    }
-    isModalOpen = true;
-    document.body.classList += " modal--open";
-    // toggle modal
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+  // toggle modal
 }
